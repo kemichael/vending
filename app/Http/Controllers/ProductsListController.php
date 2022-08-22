63453f8c1f76525c4a5e1654f31a46b38ea47396
kemichael <o.1203.kizakimegane@gmail.com>
2022-08-22 23:06:35 +0900

    giiの練習

--- app/Http/Controllers/ProductsListController.php
+++ app/Http/Controllers/ProductsListController.php
@@ -52,13 +52,5 @@ class ProductsListController extends Controller
     }
 
 
-    //詳細表示用DB処理
-    public function detail($id) {
 
-        $detail = Product::Join('companies', 'products.company_id', '=' , 'companies.id')
-        -> select('products.*', 'companies.company_name')
-        -> find($id);
-
-        return view('product_detail', compact('detail'));
-    }
 }
